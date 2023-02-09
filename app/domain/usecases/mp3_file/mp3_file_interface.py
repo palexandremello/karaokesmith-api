0dcaa9cf064c9ccf2b0872d503322c239f34286d
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-08 23:31:20 -0300

    feat: Mp3FileInterface class

---
+++ app/domain/usecases/mp3_file/mp3_file_interface.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+from domain.entities.mp3_file import Mp3File
+
+
+class Mp3FileInterface(ABC):
+
+    @abstractmethod
+    def execute(cls, path: str) -> Union[Exception, Mp3File]:
+        pass
