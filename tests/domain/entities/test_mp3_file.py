4fdc0502c09e54f75711e6184a6e809d3ca384f6
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-13 00:50:33 -0300

    tests: ensure to_dict() is returning a dictionary

--- tests/domain/entities/test_mp3_file.py
+++ tests/domain/entities/test_mp3_file.py
@@ -10,3 +10,14 @@ def test_mp3_file_from_dict():
 
     assert sut.name == mp3_dict["name"]
     assert sut.path == mp3_dict["path"]
+
+
+def test_mp3_file_to_dict():
+    mp3_dict = {"name": "森高千里 『ザ・ストレス -ストレス 中近東バージョン-』",
+                "path": "any_path"}
+
+    sut = Mp3File.from_dict(mp3_dict)
+
+    dictionaries = sut.to_dict()
+
+    assert dictionaries == mp3_dict
