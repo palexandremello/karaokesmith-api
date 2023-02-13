045d079b7424fb2166e7671a46fff5a2aca34e76
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-12 23:38:21 -0300

    tests: ensure to create Mp3File entity from dictionary

---
+++ tests/domain/entities/test_mp3_file.py
@@ -0,0 +1,12 @@
+import pytest
+
+from app.domain.entities.mp3_file import Mp3File
+
+def test_mp3_file_from_dict():
+    mp3_dict = {"name": "森高千里 『ザ・ストレス -ストレス 中近東バージョン-』",
+                "path": "any_path"}
+
+    sut = Mp3File.from_dict(mp3_dict)
+
+    assert sut.name == mp3_dict["name"]
+    assert sut.path == mp3_dict["path"]
