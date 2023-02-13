de887efc0cf72aad4caa9cb0613bf15f9e1017d1
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-13 00:51:44 -0300

    feat: Mp3FileService class

---
+++ app/domain/services/mp3_file/mp3_file_service.py
@@ -0,0 +1,16 @@
+
+from typing import Union
+from domain.services.mp3_file.mp3_file_service_interface import Mp3FileServiceInterface
+from domain.services.mp3_file.mp3_file_validator_interface import Mp3FileValidatorInterface
+
+class Mp3FileService(Mp3FileServiceInterface):
+    def __init__(self, validator: Mp3FileValidatorInterface) -> None:
+        self.validator = validator
+    
+
+    def validate_mp3_file(self, path: str) -> Union[Exception, None]:
+        try:
+            self.validator.validate(path)
+                    
+        except Exception as error:
+            return error
