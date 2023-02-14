a1a54e7b2909c211ae8b05e2a9fadfae572e398d
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-13 23:41:06 -0300

    feat: YoutubeAudio entity

--- app/domain/entities/youtube_audio.py
+++ app/domain/entities/youtube_audio.py
@@ -1,13 +1,17 @@
 import dataclasses
+from typing import Optional
+from domain.entities.mp3_file import Mp3File
 
 @dataclasses.dataclass
-class Mp3File:
-    name: str
-    path: str
+class YoutubeAudio:
+    link: str
+    mp3_file: Optional[Mp3File] = None
+
 
     @classmethod
     def from_dict(cls, dictionary: dict):
         return cls(**dictionary)
+
     
     def to_dict(self):
         return dataclasses.asdict(self)
