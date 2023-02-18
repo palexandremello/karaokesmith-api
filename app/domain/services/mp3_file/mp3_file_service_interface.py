56a50b3f569c38c649f13d1d63fff08a838d149a
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-18 12:49:54 -0300

    refactor: Mp3FileServiceInterface async method

--- app/domain/services/mp3_file/mp3_file_service_interface.py
+++ app/domain/services/mp3_file/mp3_file_service_interface.py
@@ -6,5 +6,5 @@ from typing import Union
 class Mp3FileServiceInterface(ABC):
 
     @abstractmethod
-    def validate_mp3_file(cls, path: str) -> Union[Exception, None]:
+    async def validate_mp3_file(cls, path: str) -> Union[Exception, None]:
         pass
