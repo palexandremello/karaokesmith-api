7ca7a313f11a219b00afc7d96080fa1d9cd6007e
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-18 14:28:34 -0300

    refactor: creates a class for test Mp3FileUseCase and using fixture with asyncio

--- tests/domain/usecases/mp3_file/test_mp3_file_impl.py
+++ tests/domain/usecases/mp3_file/test_mp3_file_impl.py
@@ -1,44 +1,17 @@
-
-from typing import Union
-from unittest.mock import MagicMock
-from domain.entities.mp3_file import Mp3File
+from unittest.mock import AsyncMock
+import pytest_asyncio
 from domain.services.mp3_file.mp3_file_service_interface import Mp3FileServiceInterface
-from domain.usecases.mp3_file.mp3_file_impl import Mp3FileUseCaseImpl
+from domain.usecases.mp3_file.mp3_file_impl import Mp3FileUseCase
 
 
-class Mp3FileServiceStub(Mp3FileServiceInterface):
-    def validate_mp3_file(self, name: str, path: str) -> Union[Exception, None]:
-        pass
+class TestMp3FileUseCase:
 
+    @pytest_asyncio.fixture
+    def mp3_file_service_stub(self) -> Mp3FileServiceInterface:
+        return AsyncMock(spec=Mp3FileServiceInterface)
+    
 
-def mockMp3FileServiceStub():
-    mp3_file_service_stub = Mp3FileServiceStub()
-    mp3_file_service_stub.validate_mp3_file = MagicMock(return_value=None)
-    return mp3_file_service_stub
-
-def mockExceptionMp3FileServiceStub():
-    mp3_file_service_stub = Mp3FileServiceStub()
-    mp3_file_service_stub.validate_mp3_file = MagicMock(return_value=Exception("invalid path"))
-    return mp3_file_service_stub
-
-def test_should_return_Mp3File_when_path_is_valid():
-    mp3_file_service_stub = mockMp3FileServiceStub()
-    sut = Mp3FileUseCaseImpl(mp3_file_service_stub)
-    mp3_dict = {"name": "森高千里 『ザ・ストレス -ストレス 中近東バージョン-』",
-                "path": "any_path"}
-
-    mp3_file = sut.execute(mp3_dict["name"], mp3_dict["path"])
-
-    assert isinstance(mp3_file, Mp3File)
-
-    assert mp3_file.path == mp3_dict["path"]
-
-def test_should_return_Exception_when_path_is_invalid():
-    mp3_file_service_stub = mockExceptionMp3FileServiceStub()
-    sut = Mp3FileUseCaseImpl(mp3_file_service_stub)
-    mp3_dict = {"name": "森高千里 『ザ・ストレス -ストレス 中近東バージョン-』",
-                "path": "any_path"}
-
-    error = sut.execute(mp3_dict["name"], mp3_dict["path"])
-
-    assert isinstance(error, Exception)
+    @pytest_asyncio.fixture
+    def mp3_file_usecase(self, mp3_file_service_stub):
+        return Mp3FileUseCase(mp3_file_service_stub)
+    
