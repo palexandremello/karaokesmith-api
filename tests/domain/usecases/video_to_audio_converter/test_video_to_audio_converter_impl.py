0db78007815e97556707813a2bce989d23ac5cf3
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-18 00:33:11 -0300

    tests: ensure VideoToAudioConverterUseCase returns a body with Exception when convert method throws

--- tests/domain/usecases/video_to_audio_converter/test_video_to_audio_converter_impl.py
+++ tests/domain/usecases/video_to_audio_converter/test_video_to_audio_converter_impl.py
@@ -20,7 +20,7 @@ class TestVideoToAudioConverterUseCase:
     
 
 
-    def test_should_return_an_AudioMedia_when_execute_convert_is_success(self, video_to_audio_usecase,
+    def test_should_return_an_AudioMedia_when_video_converter_is_success(self, video_to_audio_usecase,
                                                                          video_converter_service_stub):
         video = VideoSource(title="Real Estate - Paper Cup", thumbnail_url="any_thumbnail_url", path="any_path")
         audio = AudioMedia(path="any_path", audio_format= AudioFormat.MP3)
@@ -34,4 +34,16 @@ class TestVideoToAudioConverterUseCase:
         assert response.success
         assert response.body == audio
 
+    
+    def test_should_return_an_response_with_Exception_when_video_converter_has_failed(self, video_to_audio_usecase,
+                                                                                      video_converter_service_stub):
+        
+        video = VideoSource(title="Real Estate - Paper Cup", thumbnail_url="any_thumbnail_url", path="any_path")
+        error_message = "Error during audio conversion"
 
+        video_converter_service_stub.execute.side_effect = Exception(error_message)
+
+        response = video_to_audio_usecase.convert(video)
+
+        assert response.success == False
+        assert response.body == error_message
