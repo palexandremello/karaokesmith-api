7665089b304219c389e0400be7b7d778cd89fa75
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-18 14:33:38 -0300

    tests: ensure Mp3FileUseCase returns a response with success True and body with Mp3File when path is a mp3 file valid

--- tests/domain/usecases/mp3_file/test_mp3_file_impl.py
+++ tests/domain/usecases/mp3_file/test_mp3_file_impl.py
@@ -1,10 +1,15 @@
 from unittest.mock import AsyncMock
+import pytest
 import pytest_asyncio
+from domain.entities.mp3_file import Mp3File
 from domain.services.mp3_file.mp3_file_service_interface import Mp3FileServiceInterface
 from domain.usecases.mp3_file.mp3_file_impl import Mp3FileUseCase
+from domain.usecases.mp3_file.mp3_file_interface import Mp3FileUseCaseInterface
 
 
 class TestMp3FileUseCase:
+    MP3_DICT = {"name": "森高千里 『ザ・ストレス -ストレス 中近東バージョン-』",
+                      "path": "any_path"}
 
     @pytest_asyncio.fixture
     def mp3_file_service_stub(self) -> Mp3FileServiceInterface:
@@ -12,6 +17,16 @@ class TestMp3FileUseCase:
     
 
     @pytest_asyncio.fixture
-    def mp3_file_usecase(self, mp3_file_service_stub):
+    def mp3_file_usecase(self, mp3_file_service_stub: Mp3FileServiceInterface) -> Mp3FileUseCaseInterface:
         return Mp3FileUseCase(mp3_file_service_stub)
     
+    @pytest.mark.asyncio
+    async def test_should_return_Mp3File_when_path_is_valid(self, mp3_file_usecase: Mp3FileUseCaseInterface,
+                                                            mp3_file_service_stub: Mp3FileServiceInterface):
+          
+          mp3_file_service_stub.validate_mp3_file = AsyncMock(return_value=None)
+      
+          response = await mp3_file_usecase.execute(self.MP3_DICT["name"], self.MP3_DICT["path"])
+            
+          assert response.success
+          assert response.body == Mp3File.from_dict(self.MP3_DICT)
