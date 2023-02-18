60e28629f1f8d9bd7c7aa803e10358b93eaa7138
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-18 00:27:42 -0300

    feat: VideoToAudioConverter implementation class

---
+++ app/domain/usecases/video_to_audio_converter/video_to_audio_converter_impl.py
@@ -0,0 +1,22 @@
+
+
+from domain.entities.audio_media import AudioMedia
+from domain.entities.video_source import VideoSource
+from domain.services.video_converter.video_converter_service_interface import VideoConverterServiceInterface
+from domain.usecases.video_to_audio_converter.video_to_audio_converter_interface import \
+    VideoToAudioConverterUseCaseInterface
+from domain.utils.use_case_response import UseCaseResponse
+
+
+class VideoToAudioConverterUseCase(VideoToAudioConverterUseCaseInterface):
+
+    def __init__(self, video_converter_service: VideoConverterServiceInterface) -> None:
+        self.video_converter_service = video_converter_service
+
+    def convert(self, video: VideoSource) -> UseCaseResponse[AudioMedia]:
+        try:
+            audio = self.video_converter_service.execute(video)
+            return UseCaseResponse(success=True, body=audio)
+        
+        except Exception as error:
+            return UseCaseResponse(success=False, body=str(error))
