79653591a05ab26569d1fa6077f6735856ac4d86
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-19 22:35:31 -0300

    tests: ensure YoutubeVideoService get_info from valid url

---
+++ tests/domain/services/youtube_video/test_youtube_video_service.py
@@ -0,0 +1,38 @@
+
+
+from unittest.mock import AsyncMock
+import pytest
+import pytest_asyncio
+
+from domain.services.youtube_video.youtube_video_downloader_interface import YoutubeDownloaderInterface
+from domain.services.youtube_video.youtube_video_service import YoutubeVideoService
+from domain.services.youtube_video.youtube_video_service_interface import YoutubeVideoServiceInterface
+
+
+class TestYoutubeVideoService:
+
+    URL = "https://www.youtube.com/watch?v=CHKk8OozMfo"
+    EXPECTED_VIDEO_INFO = {"title": "School Food Punishment - You May Crawl",
+                           "thumbnail_url": "any_thumbnail_url"}
+
+    @pytest_asyncio.fixture
+    def youtube_video_downloader_stub(self) -> YoutubeDownloaderInterface:
+        return AsyncMock(spec=YoutubeDownloaderInterface)
+    
+
+    @pytest_asyncio.fixture
+    def youtube_video_service(self, youtube_video_downloader_stub) -> YoutubeVideoServiceInterface:
+        return YoutubeVideoService(youtube_video_downloader=youtube_video_downloader_stub)
+    
+
+
+    @pytest.mark.asyncio
+    async def test_should_be_able_to_get_video_info(self, 
+                                                    youtube_video_downloader_stub: YoutubeDownloaderInterface,
+                                                    youtube_video_service: YoutubeVideoServiceInterface):
+        
+        youtube_video_downloader_stub.get_video_info = AsyncMock(return_value=self.EXPECTED_VIDEO_INFO)
+
+        video_metadata = await  youtube_video_service.get_info(url=self.URL)
+
+        assert video_metadata == self.EXPECTED_VIDEO_INFO
