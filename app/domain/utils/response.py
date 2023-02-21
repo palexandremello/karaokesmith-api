bee1a204c9c7d273f186e225feb3c3079052f090
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-20 21:35:14 -0300

    refactor: UseCaseResponse to Response

---
+++ app/domain/utils/response.py
@@ -0,0 +1,16 @@
+
+from dataclasses import dataclass, asdict
+from typing import Generic, TypeVar, Union
+
+
+T = TypeVar("T")
+
+@dataclass
+class Response(Generic[T]):
+    success: bool
+    body: Union[T, str]
+
+
+
+    def to_dict(self):
+        return asdict(self)
