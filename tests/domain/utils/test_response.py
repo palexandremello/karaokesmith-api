ae91f8811538e2633b086215e72330aea2c2c861
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-20 21:34:47 -0300

    refactor: change test_usecase_response to test_response

---
+++ tests/domain/utils/test_response.py
@@ -0,0 +1,25 @@
+
+import dataclasses
+
+from domain.utils.response import Response
+
+@dataclasses.dataclass
+class AnyEntityClass:
+    status_code: int
+    body: str
+
+BODY = AnyEntityClass(200, "any_body")
+EXPECTED_RESPONSE = {"success": True, "body": {'status_code': 200, 'body': 'any_body'}}
+
+def test_should_be_same_instance_of_Response():
+    use_case_response = Response(success=True, body=BODY)
+
+    assert isinstance(use_case_response, Response)
+
+
+def test_should_be_able_to_return_a_dictionary_response():
+    use_case_response = Response(success=True, body=BODY)
+
+    use_case_response.body
+    response = use_case_response.to_dict()
+    assert response == EXPECTED_RESPONSE
