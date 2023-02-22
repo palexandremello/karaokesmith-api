8a0520453f12787bda6b8fe33632941f1a8fe42f
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-21 22:38:40 -0300

    refactor: formatting

--- app/domain/usecases/get_youtube_video/get_youtube_video_impl.py
+++ app/domain/usecases/get_youtube_video/get_youtube_video_impl.py
@@ -1,5 +1,3 @@
-
-
 from domain.entities.video_source import VideoSource
 from domain.usecases.get_youtube_video.get_youtube_video_interface import GetYoutubeVideoUseCaseInterface
 from domain.services.youtube_video.youtube_video_service_interface import YoutubeVideoServiceInterface
@@ -7,7 +5,6 @@ from domain.utils.response import Response
 
 
 class GetYoutubeVideoUseCase(GetYoutubeVideoUseCaseInterface):
-
     def __init__(self, youtube_video_service: YoutubeVideoServiceInterface) -> None:
         self.youtube_video_service = youtube_video_service
 
@@ -16,6 +13,6 @@ class GetYoutubeVideoUseCase(GetYoutubeVideoUseCaseInterface):
             video = await self.youtube_video_service.download(video_url)
 
             return Response(success=True, body=video)
-        
+
         except Exception as error:
             return Response(success=False, body=str(error))
