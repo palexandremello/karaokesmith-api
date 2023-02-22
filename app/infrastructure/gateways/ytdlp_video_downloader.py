49cd770acfdee6c9b2c5b83d2e2cdd719046b1ff
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-21 22:05:54 -0300

    feat: ensure YtDlpVideoDownloader returns a filepath when get_video is successful

--- app/infrastructure/gateways/ytdlp_video_downloader.py
+++ app/infrastructure/gateways/ytdlp_video_downloader.py
@@ -11,19 +11,29 @@ from domain.services.youtube_video.youtube_video_downloader_interface import (
 class YtDlpVideoDownloader(YoutubeDownloaderInterface):
     def __init__(self) -> None:
         self._tmp_dir = TemporaryDirectory().name
-        self.__options = {"outtmpl": f"{self._tmp_dir}/%(id)s%(ext)s"}
+        self.__options = {"outtmpl": f"{self._tmp_dir}/%(title)s%(ext)s"}
         self.youtube_dl: YoutubeDL = YoutubeDL(self.__options)
 
     async def get_video(self, video_url: str) -> str:
-        pass
+        with self.youtube_dl:
+            result = await asyncio.get_event_loop().run_in_executor(
+                None, lambda: self.youtube_dl.download([video_url])
+            )
+
+            if len(result) == 0:
+                raise FileExistsError("it was not able to retrieve video content")
+
+            return result[0]["filepath"]
 
     async def get_video_info(self, video_url: str) -> VideoMetadata:
         with self.youtube_dl:
             result = await asyncio.get_event_loop().run_in_executor(
                 None, lambda: self.youtube_dl.extract_info(video_url, download=False)
             )
+            video_title = result.get("title", None)
+            video_thumbnail = result.get("thumbnail", None)
 
-        if not result.keys():
+        if video_thumbnail is None or video_title is None:
             raise KeyError("any video metadata")
 
-        return VideoMetadata(title=result["title"], thumbnail_url=result["thumbnail"])
+        return VideoMetadata(title=video_title, thumbnail_url=video_thumbnail)
--- tests/infrastructure/gateways/test_ytdlp_video_downloader.py
+++ tests/infrastructure/gateways/test_ytdlp_video_downloader.py
@@ -10,6 +10,10 @@ class TestYtDlpVideoDownloader:
     def mock_youtube_dl(self):
         return {"title": "mocked_video_title", "thumbnail": "mocked_thumbnail_url"}
 
+    @pytest.fixture
+    def mock_download(self):
+        return [{"filepath": "any_path"}]
+
     @pytest_asyncio.fixture
     def ytdlp_video_downloader_stub(self) -> YtDlpVideoDownloader:
         return YtDlpVideoDownloader()
@@ -36,3 +40,15 @@ class TestYtDlpVideoDownloader:
         with patch("yt_dlp.YoutubeDL.extract_info", return_value={}):
             with pytest.raises(KeyError, match="any video metadata"):
                 await ytdlp_video_downloader_stub.get_video_info("any_video_url")
+
+    @pytest.mark.asyncio
+    async def test_should_be_able_to_returns_a_filepath_when_get_video_is_successful(
+        self,
+        ytdlp_video_downloader_stub: YtDlpVideoDownloader,
+    ):
+        with patch(
+            "yt_dlp.YoutubeDL.download", return_value=[{"filepath": "any_path"}]
+        ):
+            path = await ytdlp_video_downloader_stub.get_video("any_video_url")
+
+            assert path == "any_path"
