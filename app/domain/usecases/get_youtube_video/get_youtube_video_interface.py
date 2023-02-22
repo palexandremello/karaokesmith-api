457c2a590a0b9699a256d49a39d77b4c43cc8ed2
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-21 23:24:22 -0300

    refactor: formatting black

--- app/domain/usecases/get_youtube_video/get_youtube_video_interface.py
+++ app/domain/usecases/get_youtube_video/get_youtube_video_interface.py
@@ -5,7 +5,6 @@ from domain.utils.response import Response
 
 
 class GetYoutubeVideoUseCaseInterface(ABC):
-
     @abstractmethod
     async def get(self, video_url: str) -> Response[VideoSource]:
         pass
