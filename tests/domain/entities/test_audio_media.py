c088259c7c486c21d995ee0a40a2d710d9beb27b
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-23 00:49:03 -0300

    refactor: formatting tests

--- tests/domain/entities/test_audio_media.py
+++ tests/domain/entities/test_audio_media.py
@@ -1,20 +1,17 @@
-
 from domain.entities.audio_media import AudioMedia
 
 
-AUDIO_MEDIA_DICTIONARY = {"path": "any_path", "audio_format": "mp3" }
+AUDIO_MEDIA_DICTIONARY = {"path": "any_path", "audio_format": "mp3"}
+
 
 def test_should_create_a_AudioMedia_from_dict():
-
     sut = AudioMedia.from_dict(AUDIO_MEDIA_DICTIONARY)
 
     assert sut.path == AUDIO_MEDIA_DICTIONARY["path"]
     assert sut.audio_format == AUDIO_MEDIA_DICTIONARY["audio_format"]
 
 
-
 def test_should_returns_a_dictionary_from_AudioMedia_entity():
-
     sut = AudioMedia.from_dict(AUDIO_MEDIA_DICTIONARY)
 
     audio_media_dict = sut.to_dict()
