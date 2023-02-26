9f76e5d042cde5d3c318df2bd270cc7c54bf34d2
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-02-26 00:16:25 -0300

    feat: CreateSampleFromYoutubeUseCase is now responsible for create a sample when video_url is provided by user

---
+++ app/domain/usecases/create_sample_from_youtube/create_sample_from_youtube_impl.py
@@ -0,0 +1,30 @@
+from domain.entities.sample import Sample
+from domain.services.create_sample_service.create_sample_service_interface import CreateSampleServiceInterface
+from domain.usecases.create_sample_from_youtube.create_sample_from_youtube_interface import (
+    CreateSampleFromYoutubeUseCaseInterface,
+)
+from domain.usecases.youtube_audio.youtube_audio_interface import YoutubeAudioUseCaseInterface
+from domain.utils.response import Response
+
+
+class CreateSampleFromYoutubeUseCase(CreateSampleFromYoutubeUseCaseInterface):
+    def __init__(
+        self,
+        youtube_audio_usecase: YoutubeAudioUseCaseInterface,
+        create_sample_service: CreateSampleServiceInterface,
+    ) -> None:
+        self.youtube_audio_usecase = youtube_audio_usecase
+        self.create_sample_service = create_sample_service
+
+    async def execute(self, video_url: str) -> Response[Sample]:
+        youtube_audio_response = await self.youtube_audio_usecase.execute(video_url)
+
+        if not youtube_audio_response.success:
+            return Response(success=False, body=youtube_audio_response.body)
+
+        sample_response = await self.create_sample_service.execute(youtube_audio_response.body.mp3_file)
+
+        if not sample_response.success:
+            return Response(success=False, body=sample_response.body)
+
+        return Response(success=True, body=sample_response.body)
