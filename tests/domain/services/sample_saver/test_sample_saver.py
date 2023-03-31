23cced15a1d00f3e9e58252eea6007f90f74ebb5
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-03-31 02:31:02 -0300

    tests: ensure SampleSaver returns a response with error when save_method throws

--- tests/domain/services/sample_saver/test_sample_saver.py
+++ tests/domain/services/sample_saver/test_sample_saver.py
@@ -27,3 +27,13 @@ class TestSampleSaver:
 
         assert response.success
         assert response.body.path == "any_path"
+
+    def test_should_be_able_to_response_with_error_when_save_throws(self, save_method_stub, sample_saver):
+        sample = Sample(audio_option=Mp3File(name="any_name", path="any_path"), content=b"any_bytes")
+        expected_sample = sample
+        expected_sample.path = "any_path"
+        save_method_stub.save.return_value = Response(success=False, body="any_exception")
+        response = sample_saver.save_sample(sample)
+
+        assert not response.success
+        assert response.body == "any_exception"
