9411b26dee7f94ec99b004f3d6eacaeab370ac38
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-21 19:11:12 -0300

    refactor: refactoring imports and formatting files

--- tests/domain/utils/test_service_response.py
+++ tests/domain/utils/test_service_response.py
@@ -1,25 +1,22 @@
 import pytest
-from domain.utils.service_response import ServiceResponse
+from app.domain.utils.service_response import ServiceResponse
 
 
 class TestServiceResponse:
-
     @pytest.fixture
     def error(self) -> dict:
         return {"success": False, "error_message": "Any Error"}
-    
+
     @pytest.fixture
     def successful(self) -> dict:
         return {"success": True, "error_message": None}
-    
+
     def test_should_be_able_to_return_a_dict_response_when_service_throws_error(self, error):
         sut = ServiceResponse(success=error["success"], error_message=error["error_message"])
 
         dict_to_be_assert = sut.to_dict()
 
         assert dict_to_be_assert == error
-
-
 
     def test_should_be_able_to_return_a_dict_response_when_service_is_successful(self, successful):
         sut = ServiceResponse(success=successful["success"], error_message=successful["error_message"])
