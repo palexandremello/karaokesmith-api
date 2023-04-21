29174e4fc36ab90c77993fd3dc1fced7b22ca2ea
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-21 19:10:53 -0300

    refactor: refactoring imports

--- tests/domain/utils/test_response.py
+++ tests/domain/utils/test_response.py
@@ -1,15 +1,17 @@
-
 import dataclasses
 
-from domain.utils.response import Response
+from app.domain.utils.response import Response
+
 
 @dataclasses.dataclass
 class AnyEntityClass:
     status_code: int
     body: str
 
+
 BODY = AnyEntityClass(200, "any_body")
-EXPECTED_RESPONSE = {"success": True, "body": {'status_code': 200, 'body': 'any_body'}}
+EXPECTED_RESPONSE = {"success": True, "body": {"status_code": 200, "body": "any_body"}}
+
 
 def test_should_be_same_instance_of_Response():
     use_case_response = Response(success=True, body=BODY)
