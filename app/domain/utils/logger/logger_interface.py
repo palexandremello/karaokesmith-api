6516a5dad1a83731ab72bd6cced4cedb59d997b4
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-22 03:05:27 -0300

    feat: LoggerInterface

---
+++ app/domain/utils/logger/logger_interface.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class LoggerInterface(ABC):
+    @abstractmethod
+    def debug(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def info(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def warning(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def error(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def critical(self, message: str) -> None:
+        pass
