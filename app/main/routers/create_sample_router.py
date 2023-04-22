b05325ca7ea42b161d6287dfe8504aa08f489770
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-22 03:14:11 -0300

    feat: CreateSampleRouter

---
+++ app/main/routers/create_sample_router.py
@@ -0,0 +1,22 @@
+from app.main.adapters.starlette_adapter import starlette_adapter
+from starlette.responses import JSONResponse
+
+from app.main.composite.create_sample_composite import create_sample_composer
+from app.main.adapters.starlette_request_adapter import StarletteRequestAdapter
+
+
+async def create_sample(request):
+    message = {}
+
+    response = await starlette_adapter(
+        request=request, api_controller=create_sample_composer(), request_adapter=StarletteRequestAdapter()
+    )
+
+    if response.status_code < 300:
+        message = {"type": "spreadsheet", "body": response.body}
+        return JSONResponse(message, status_code=response.status_code)
+
+    return JSONResponse(
+        {"error": {"status": response.status_code, "title": response.body["error"]}},
+        status_code=response.status_code,
+    )
