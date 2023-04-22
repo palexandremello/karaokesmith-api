eb5ce7da9afbc166b6361f85a765d89e6f03e836
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-22 03:12:37 -0300

    feat: server configuration from starlette

---
+++ app/main/config/server.py
@@ -0,0 +1,10 @@
+from starlette.applications import Starlette
+from app.main.middlewares import middlewares
+from app.main.routers import routes
+
+
+def startup():
+    print("Go on!!")
+
+
+app = Starlette(debug=True, routes=routes, on_startup=[startup], middleware=middlewares)
